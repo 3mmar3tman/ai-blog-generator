d642419a2b9de8fb43a52841a8c856994e91b2d8
Ammar Yasser <3mmar3tman@example.com>
2025-11-12 21:23:12 +0200

    Add initial server setup and configuration files

---
+++ server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import mongoose, { connect } from "mongoose";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+
+connectDB();
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
